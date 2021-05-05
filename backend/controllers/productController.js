6f843d787a6e112b6658a072e45c5ff4c5264a68
hammadweb2020 <78916257+hammadweb2020@users.noreply.github.com>
2021-05-05 12:04:43 +0400

    Clean Up By Using Controllers
    Clean Up By Using Controllers - manager all request through controller folder

---
+++ backend/controllers/productController.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel'
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({})
+  res.json(products)
+})
+
+// @desc    Fetch sigle product
+// @route   GET /api/product/:id
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+    //  .json({ message: 'Product not found' })
+  }
+  //const product = products.find((p) => p._id === req.params.id)
+})
+
+export { getProducts, getProductById }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,36 +1,8 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel'
 const router = express.Router()
+import { getProducts, getProductById } from '../controllers/productController'
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-
-    res.json(products)
-  })
-)
-
-// @desc    Fetch sigle product
-// @route   GET /api/product/:id
-// @access  Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-      //  .json({ message: 'Product not found' })
-    }
-    //const product = products.find((p) => p._id === req.params.id)
-  })
-)
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 export default router
