8aa2d78e47ef7f663a9c8dcf54f233c324ca24e9
hammadweb2020 <78916257+hammadweb2020@users.noreply.github.com>
2021-05-09 16:49:50 +0400

    Shipping Screen & Save Address
    Shipping Screen & Save Address

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`)
@@ -26,4 +30,13 @@ export const removeFromCart = (id) => (dispath, getState) => {
   })
 
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const saveShippingAddress = (data) => (dispath) => {
+  dispath({
+    type: CART_SAVE_SHIPPING_ADDRESS,
+    payload: data,
+  })
+
+  localStorage.setItem('cartItems', JSON.stringify(data))
 }
