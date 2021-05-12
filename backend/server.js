76170042f58fb916ca2d559b5ba6a7f75f24697f
hammadweb2020 <78916257+hammadweb2020@users.noreply.github.com>
2021-05-12 15:02:26 +0400

    Backend Order Controller & Route
    Backed Order Controller & Route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware'
 import productRoutes from './routes/productRoutes'
 import userRoutes from './routes/userRoutes'
+import orderRoutes from './routes/orderRoutes'
 
 dotenv.config()
 
@@ -21,6 +22,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
