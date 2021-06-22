6e00f916a605674fbd22a4c7ead1b7700fd4876c
hammadweb2020 <78916257+hammadweb2020@users.noreply.github.com>
2021-06-22 13:46:56 +0400

    Update User Functionality

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -5,43 +5,64 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUser } from '../actions/userActions'
+import { USER_UPDATE_RESET } from '../constants/userConstants'
 
 const UserEditScreen = ({ match, history }) => {
  const userId = match.params.id
  
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+
   const [isAdmin, setIsAdmin] = useState(false)
-  const [message, setMessage] = useState(null)
+
 
   const dispatch = useDispatch()
-  const userDetails = useSelector((state) => state.userDetails)
 
+  const userDetails = useSelector((state) => state.userDetails)
   const { loading, error, user } = userDetails
+  const userUpdate = useSelector((state) => state.userUpdate)
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = userUpdate
 
 
   useEffect(() => {
 
-if(!user.name || user._id !== userId)
-{
-dispatch(getUserDetails(userId))
-}
-else {
 
-setName(user.name)
-setEmail(user.email)
-setIsAdmin(user.isAdmin)
+    if(successUpdate){
+dispatch({type: USER_UPDATE_RESET})
+history.push('/admin/userlist')
 
-}
+    }
+
+    else {
 
 
-  }, [dispatch,userId,user])
+          if(!user.name || user._id !== userId)
+          {
+          dispatch(getUserDetails(userId))
+          }
+          else {
+          
+          setName(user.name)
+          setEmail(user.email)
+          setIsAdmin(user.isAdmin)
+          
+          }
+
+
+    }
+
+
+
+  }, [dispatch, history, userId, user, successUpdate])
 
   const submitHandler = (e) => {
     e.preventDefault()
-  
+  dispatch(updateUser({ _id: userId, name, email, isAdmin}))
   }
 
   return (
@@ -51,6 +72,10 @@ setIsAdmin(user.isAdmin)
 
 <FormContainer>
       <h1>Edit User</h1>
+{loadingUpdate && <Loader />}
+{errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
+
+
 { loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
 
 <Form onSubmit={submitHandler}>
