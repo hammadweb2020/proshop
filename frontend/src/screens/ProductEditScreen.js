7a75fae778cfda240b616df93fb8734a395f42a2
hammadweb2020 <78916257+hammadweb2020@users.noreply.github.com>
2021-07-13 11:13:10 +0400

    Update ProductEditScreen.js

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -29,7 +29,7 @@ const ProductEditScreen = ({ match, history }) => {
 
 
   const productUpdate = useSelector((state) => state.productUpdate)
-  const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = productUpdate
+  const { loading: loadingUpdate, error:  errorUpdate, success: successUpdate } = productUpdate
 
 
 
