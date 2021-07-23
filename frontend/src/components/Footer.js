da66c4930863a95d79eb11eef530eae23d899406
hammadweb2020 <78916257+hammadweb2020@users.noreply.github.com>
2021-07-23 23:13:33 +0400

    link update

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
                         
                     </Row>
                     <Row>
-                          <Col className='text-center py-1'><Link to='https://github.com/hammadweb2020'><i className="fab fa-github" style={{fontSize:'24px'}}></i></Link> &nbsp; <Link to='http://linkedin.com/in/muhammad-hammad-siddique-a4a54765/'><i className="fab fa-linkedin" style={{fontSize:'24px'}}></i></Link> &nbsp; <Link to='https://www.behance.net/hammadweb'><i className="fab fa-behance" style={{fontSize:'24px'}}></i></Link></Col>  
+                          <Col className='text-center py-1'><a href='https://github.com/hammadweb2020' target='_blank' rel="noreferrer"><i className="fab fa-github" style={{fontSize:'24px'}}></i></a> &nbsp; <a target='_blank' href='http://linkedin.com/in/muhammad-hammad-siddique-a4a54765/' rel="noreferrer"><i className="fab fa-linkedin" style={{fontSize:'24px'}}></i></a> &nbsp; <a target='_blank' href='https://www.behance.net/hammadweb' rel="noreferrer"><i className="fab fa-behance" style={{fontSize:'24px'}}></i></a></Col>  
                         
                     </Row>
 
